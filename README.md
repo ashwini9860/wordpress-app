00056a471f5a04fe4cffff8bae16dde5ab6ecedd
Your Name <you@example.com>
2021-03-08 09:03:42 +0000

    updated code

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # wordpress-app
 1. It create wordpress docker image and push it to docker hub using Jenkins
 
-2. . It has terraform to deploy container on AWS using fargate 
-   
-   a. creates ALB 
+2. . It has terraform to deploy container on AWS using fargate
+ 
+   a. VPC with public subnet, internet gateway and route table
    
    b. creates efs
    
-   c. module itslef creates IAM role 
+   c. create efs mount point
    
-   d. module itself creates cloudwatch resouce for logging
+   d. create IAM role with policy 
+
+   e. create security group with port 80,22,2049 open to all 
    
-   e. creates terraform ecs cluster 
+   f. create ECS cluster
+ 
+   g. create ecs task defination
+
+   h. run ecs service with fargate
+ 
+   i. create ECS autoscalling
--- terraform/main.tf
+++ terraform/main.tf
@@ -98,62 +98,6 @@ resource "aws_route_table_association" "public-rt-association" {
   route_table_id = "${aws_route_table.public-rt.id}"
 }
 
-########### create efs #######
-resource "aws_efs_file_system" "efs" {
-  creation_token = "clever"
-
-  tags = {
-    Name = "${var.project}_EFS"
-  }
-}
-
-######## create ecs cluster #######
-resource "aws_ecs_cluster" "ecs_cluster" {
-  name = "${var.project}_ecs"
-}
-
-########## 
-resource "aws_ecs_task_definition" "clever_task" {
-  family                   = "test"
-  task_role_arn            = aws_iam_role.deployment_access_role.arn
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "0.25vCPU"
-  memory                   = "0.5GB"
-  network_mode             = "awsvpc"
-  container_definitions    = <<TASK_DEFINITION
-[
-    {
-        
-        "environment": [
-            {"name": "WORDPRESS_DB_PASSWORD", "value": "wordpress"},
-            {"name": "WORDPRESS_DB_USER", "value": "wordpress"},
-            {"name": "WORDPRESS_DB_NAME", "value": "wordpress"},
-            {"name": "WORDPRESS_DB_HOST", "value": "db:3306"}
-        ],
-        "essential": true,
-        "image": "auchoudhari/wordpress-testlatest",
-        "name": "app",
-        "memory": 128,
-        "portMappings": [
-            {
-                "containerPort": 80
-            }
-        ]
-        
-    }
-]
-TASK_DEFINITION
-
-  volume {
-    name = "${var.project}-storage"
-
-    efs_volume_configuration {
-      file_system_id = aws_efs_file_system.efs.id
-      root_directory = "/wp-content/uploads/"
-    }
-  }
-}
-
 ########### SG ########################
 resource "aws_security_group" "dynamic_sg" {
   name        = "${var.project}_sg"
@@ -183,13 +127,85 @@ resource "aws_security_group" "dynamic_sg" {
 
 }
 
-data "aws_ecs_task_definition" "main" {
-  task_definition = aws_ecs_task_definition.clever_task.family
+########### create efs #######
+resource "aws_efs_file_system" "efs" {
+  creation_token = "clever"
+
+  tags = {
+    Name = "${var.project}_EFS"
+  }
 }
+
+###### create efs mount #########
+resource "aws_efs_mount_target" "this" {
+
+  file_system_id  = aws_efs_file_system.efs.id
+  security_groups = [aws_security_group.dynamic_sg.id]
+  subnet_id       = aws_subnet.public_subnet[0].id
+}
+
+######## create ecs cluster #######
+resource "aws_ecs_cluster" "ecs_cluster" {
+  name = "${var.project}_ecs"
+}
+
+########## 
+resource "aws_ecs_task_definition" "clever_task" {
+  family                   = "cleverapp"
+  task_role_arn            = aws_iam_role.deployment_access_role.arn
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "0.25vCPU"
+  memory                   = "0.5GB"
+  network_mode             = "awsvpc"
+  container_definitions    = <<TASK_DEFINITION
+[
+    {
+        
+        "environment": [
+            {"name": "WORDPRESS_DB_PASSWORD", "value": "wordpress"},
+            {"name": "WORDPRESS_DB_USER", "value": "wordpress"},
+            {"name": "WORDPRESS_DB_NAME", "value": "wordpress"},
+            {"name": "WORDPRESS_DB_HOST", "value": "db:3306"}
+        ],
+        "essential": true,
+        "mountPoints": [
+                {
+                    "containerPath": "/wp-content/plugins/",
+                    "sourceVolume": "${var.project}-storage"
+                }
+            ],
+        "image": "auchoudhari/wordpress-test:latest",
+        "name": "app",
+        "memory": 128,
+        "portMappings": [
+            {
+                "containerPort": 80,
+                "hostPort": 80
+            }
+        ]
+        
+    }
+]
+TASK_DEFINITION
+
+  volume {
+    name = "${var.project}-storage"
+
+    efs_volume_configuration {
+      file_system_id = aws_efs_file_system.efs.id
+      #root_directory = "/wp-content/plugins/"
+    }
+  }
+}
+
+
+#data "aws_ecs_task_definition" "main" {
+#  task_definition = aws_ecs_task_definition.clever_task.family
+#}
 
 #####
 resource "aws_ecs_service" "service" {
-  name             = "test"
+  name             = "cleverapp"
   cluster          = aws_ecs_cluster.ecs_cluster.id
   task_definition  = aws_ecs_task_definition.clever_task.family
   desired_count    = 1
@@ -197,9 +213,60 @@ resource "aws_ecs_service" "service" {
   launch_type      = "FARGATE"
   network_configuration {
     security_groups  = [aws_security_group.dynamic_sg.id]
-    subnets          = [aws_subnet.public_subnet[0].id, aws_subnet.public_subnet[1].id, aws_subnet.public_subnet[2].id]
+    subnets          = [aws_subnet.public_subnet[0].id]
     assign_public_ip = "true"
   }
 
 }
 
+#### ecs autoscalling target #####
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 4
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.ecs_cluster.name}/${aws_ecs_service.service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+######## auto scalling up policy #######
+resource "aws_appautoscaling_policy" "scale_up" {
+  name               = "clever-tap-scale-up"
+  policy_type        = "StepScaling"
+  resource_id        = "service/${aws_ecs_cluster.ecs_cluster.name}/${aws_ecs_service.service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+
+  step_scaling_policy_configuration {
+    cooldown                = 60
+    adjustment_type         = "ChangeInCapacity"
+    metric_aggregation_type = "Average"
+    step_adjustment {
+      metric_interval_upper_bound = 0
+      scaling_adjustment          = -1
+    }
+
+  }
+
+}
+
+##### cloudwatch alarm #######
+resource "aws_cloudwatch_metric_alarm" "high" {
+
+  alarm_name          = "clever-alarm-high"
+  alarm_description   = "Alarm monitors high utilization for scaling up"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = "2"
+  threshold           = "60"
+  period = "60"
+  statistic           = "Average"
+  alarm_actions       = [aws_appautoscaling_policy.scale_up.arn]
+  metric_name         = "CPUReservation"
+  namespace           = "AWS/ECS"
+  dimensions = {
+    ClusterName = "${aws_ecs_cluster.ecs_cluster.name}"
+  }
+  insufficient_data_actions = []
+  ok_actions = []
+  actions_enabled = true
+  datapoints_to_alarm = 1
+}
